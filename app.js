46884d388077b578f44f5726e6887560a0bfca22
mehulraj19 <mehul.raj2018@vitstudent.ac.in>
2020-07-31 23:33:35 +0530

    Version 3 with Google SignIn

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -33,27 +34,57 @@ mongoose.set("useCreateIndex",true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 // userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:['password']});
 
 const User = new mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user,done){
+  done(null,user.id);
+});
+passport.deserializeUser(function(id,done){
+  User.findById(id, function(err,user){
+    done(err,user);
+  });
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb){
+    console.log(profile);
+    User.findOrCreate({googleId: profile.id},function(err,user){
+      return cb(err,user);
+    });
+  }
+));
+
 
 app.get("/",function(req,res){
   res.render("home");
-})
+});
+app.get("/auth/google",
+  passport.authenticate("google",{scope: ["profile"]})
+);
+app.get("/auth/google/secrets",
+  passport.authenticate("google",{failureRedirect:"/login"}),
+  function(req,res){
+    res.redirect("/secrets");
+  });
+
 app.get("/login",function(req,res){
   res.render("login");
-})
+});
 app.get("/register",function(req,res){
   res.render("register");
-})
+});
 app.get("/secrets",function(req,res){
   if(req.isAuthenticated()){
     res.render("secrets");
@@ -104,4 +135,4 @@ app.post("/login",function(req,res){
 
 app.listen(3000,function(){
   console.log("The server is running in port 3000.");
-})
+});
